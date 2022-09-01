44d6c84b45f2c8bd240a0931350690b87a897f91
mosspaul <gmosspaul18@gmail.com>
2022-09-01 10:27:44 -0500

    Pass first test, removes part of array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removable) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === removable) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 
 };
 
