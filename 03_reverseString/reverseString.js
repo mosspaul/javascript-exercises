b4d51f11a7fff5773268f41fa285a15bab595302
mosspaul <gmosspaul18@gmail.com>
2022-09-01 10:20:37 -0500

    Test and refactor the reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    list = [];
+    for (let i = string.length; i >= 0; i--) {
+        list.push(string[i]);
+    }
+    return list.join("");
 };
 
 // Do not edit below this line
