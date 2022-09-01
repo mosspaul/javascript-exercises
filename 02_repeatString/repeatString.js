531722ddef7deeea1bb01e02ecd843279f12e6de
mosspaul <gmosspaul18@gmail.com>
2022-09-01 10:10:35 -0500

    Test and refactor repeatString to work right

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    list = [];
+    for (let i = 0; i < num; i++) {
+        list.push(string);
+    }
+    return list.join("");
 };
 
 // Do not edit below this line
