9365551591ae9e8784e168b72736955cba05d053
mosspaul <gmosspaul18@gmail.com>
2022-09-02 09:57:46 -0500

    Refactor removeFromArray for multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
-const removeFromArray = function(array, removable) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === removable) {
-            array.splice(i, 1);
+const removeFromArray = function(array, ...args) {
+    for (const arg in args) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === args[arg]) {
+                array.splice(i, 1);
+            }
         }
     }
     return array;
-
 };
+removeFromArray([1, 2, 3, 4], 1, 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
