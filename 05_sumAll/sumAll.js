5e4607cd3a3f08bbfaef5a7bf4aa00bd2e8fc2a0
mosspaul <gmosspaul18@gmail.com>
2022-09-02 10:34:37 -0500

    Create sumAll function with error handling

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!(typeof(a) === "number")||!(typeof(b) === "number")) {
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR"
+    } else if (a > b) {
+        higher = a;
+        lower = b;
+    } else if (b > a) {
+        higher = b;
+        lower = a;
+    }
+    let sum = 0;
+    for (let i = lower; i <= higher; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
