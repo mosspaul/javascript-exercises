3cd70b2e4106dce7088dfed2421aea98bdbc4124
mosspaul <gmosspaul18@gmail.com>
2022-09-08 12:28:55 -0500

    Develop and test calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function(num1, num2) {
-	return num1 + num2;
+const add = (num1, num2) => num1 + num2;
+
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = function(array) {
+	let sum = 0;
+  for (let num of array) {
+    sum += num;
+  }
+  return sum;
 };
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
+const multiply = function(array) {
+  let product = 1;
+  for (let num of array) {
+    product *= num;
+  }
+  return product;
 };
 
-const sum = function() {
+const power = function(num1, num2) {
+  product = 1;
+	for (let i = 0; i < num2; i++) {
+    product *= num1;
+  }
+  return product;
+};
+/*
+*
+*
+*/
+const factorial = (n) => {
+  let product = 1;
+  if (n < 1) {
+    return product;
+  }
+  while(n > 1) {
+    product = n * factorial(n - 1);
+    break;
+  }
+  return product;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
