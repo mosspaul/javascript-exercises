22519206a26f0d07f26bdad89018a1ab950e59fd
mosspaul <gmosspaul18@gmail.com>
2022-09-08 13:23:27 -0500

    Finish making palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let text = string.toLowerCase();
+    list = [];
+    textList = [];
+    for (let i = text.length - 1; i >= 0; i--) {
+        if (isCharacterALetter(text[i])) {
+            list.push(text[i]);
+            textList.push(text[i]);
+        }
+    }
+    let str = list.join("").toLowerCase();
+    let newText = textList.reverse().join("").toLowerCase();
+    console.log(`Original text: ${newText}\n Current text: ${str}!`);
+    return str === newText ? true : false;
 };
 
+function isCharacterALetter(char) {
+    return (/[a-zA-Z]/).test(char)
+}
+
+palindromes("race car!");
 // Do not edit below this line
 module.exports = palindromes;
