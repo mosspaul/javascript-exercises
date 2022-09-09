3c73a6b938444085a78575981446bebcabe4c65c
mosspaul <gmosspaul18@gmail.com>
2022-09-09 10:35:19 -0500

    Create findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    return persons.reduce((oldest, current) => {
+        old = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        now = getAge(current.yearOfBirth, current.yearOfDeath);
+        return old < now ? current : oldest;
 
-};
+        
+    });
+    
+}
+function getAge(alive, dead) {
+    // if no dead, dead becomes the current year, passes test one and two becuase both are checked
+    if (!dead) {
+        dead = new Date().getFullYear();
+        return dead - alive;
+    }
+    return dead - alive;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
