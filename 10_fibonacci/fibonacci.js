e00525e80352d2b13311d2749c300a8c8a632a55
mosspaul <gmosspaul18@gmail.com>
2022-09-09 09:23:43 -0500

    Create fibonacci function v.1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let i = 1;
+    let n = 0;
+    let list = [0, 1];
+    while (i <= num) {
+        list.push(fib(n));
+        n++;
+        i++;
+    }
+    function fib(n) {
+        return list[n] + list[n + 1];
+    }
+    return list[num];
 };
 
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
